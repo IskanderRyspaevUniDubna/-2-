cde9f7dc86fc46b7163756ea69d48a8fe42bf81b
IskanderRyspaevUniDubna <83295913+IskanderRyspaevUniDubna@users.noreply.github.com>
2021-09-24 16:50:02 +0300

    Add files via upload

---
+++ Lesson_1/Lesson_1/Lesson_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+#include "MySin.h"
+using namespace std;
+int main()
+{
+    double xmin, xmax, dx = 0.05, x;
+    int n;
+    xmin = -3.1415926;
+    xmax = 3.1415926;
+    n = (int)((xmax - xmin) / dx) + 1;
+    for (int i = 0; i < n; i++) 
+    {
+        x = xmin + i * dx;
+        cout << x << " \t " << Sin(x) << " \t " << sin(x) << "\n";
+    }
+    return 0;
+}
